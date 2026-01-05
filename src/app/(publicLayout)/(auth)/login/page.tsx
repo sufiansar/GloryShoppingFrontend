41d111a56877b8b5258adc32c178617a6926bb51
sufiansar <sufiancodecrush@gmail.com>
2026-01-06 01:13:54 +0600

    Initialized the Glory Project

---
+++ src/app/(publicLayout)/(auth)/login/page.tsx
@@ -0,0 +1,9 @@
+const Loginpage = () => {
+  return (
+    <div>
+      <h1>Welcome to the Loginpage Component!</h1>
+    </div>
+  );
+};
+
+export default Loginpage;
---
+++ src/app/(publicLayout)/(auth)/register/page.tsx
@@ -0,0 +1,9 @@
+const RegisterPage = () => {
+  return (
+    <div>
+      <h1>Welcome to the RegisterPage Component!</h1>
+    </div>
+  );
+};
+
+export default RegisterPage;
